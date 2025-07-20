199b049dea2ce0ebe5330a5af7cde28bd5a0089d
PROSUN619 <prosun.chakraborty92@gmail.com>
2025-07-20 12:25:18 +0530

    creating custom effect -delayed using runInInjectionContext

--- src/app/components/signal-ex/signal-ex.ts
+++ src/app/components/signal-ex/signal-ex.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Signal, signal, WritableSignal } from '@angular/core';
+import { Component, computed, effect, Injector, runInInjectionContext, Signal, signal, WritableSignal } from '@angular/core';
 
 @Component({
   selector: 'app-signal-ex',
@@ -8,56 +8,28 @@ import { Component, computed, Signal, signal, WritableSignal } from '@angular/co
 })
 export class SignalEx {
 
-  // courseName = signal<string>('Angular Fundamentals');
-  // courseDuration = signal<number>(40);
-  // couseDetails = computed(() => {
-  //   return `${this.courseName()} - Duration: ${this.courseDuration()} hours` 
-  // } );
 
   count: WritableSignal<number> = signal(0);
-  doubleCount: Signal<number> = computed(() => this.count() * 2);
-  showCount = signal(false);
 
+  
 
-  conditionalCount = computed(() => {
-    console.log('Conditional count running'); 
-    if (!this.showCount()) {
-      return "nothing to show here";
-    }else {
-      return "The count value is: " + this.count();
-    }
-  });
-
-  getCondition(){
-    console.log(this.conditionalCount());
+  constructor(private injector : Injector) {
   }
 
-  expensiveComputation = computed(() => {
-    console.log('Expensive computation running'); 
-    return this.count() * 100;
-  });
-
-  expensiveCount(){
-    console.log('Expensive count running');
-    console.log(this.expensiveComputation());
+  ngOnInit() {
   }
 
   updateCount() {
     this.count.update(value => value + 1);
   }
 
-  updateSHowCount() {
-    this.showCount.update(value => !value); 
+  initializeLogging() {
+    runInInjectionContext(this.injector,() => {
+   
+    effect(() => {
+      console.log('Count changed:', this.count());
+    });
   }
-  
-
-  constructor() {
-    // Initialization logic can go here
-  }
-
-  ngOnInit() {
-    //console.log('beforeSet', this.doubleCount());
-    //this.count.set(5);
-    //console.log('afterSet', this.doubleCount());
-  }
+  );
+}
 }
